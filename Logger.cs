7e7ad83ef2d6d3f19e2057bc955ac9e024c608e2
ankatus <antti.j.kataja@gmail.com>
2020-12-06 18:18:46 +0200

    Added logger

---
+++ Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace dsproject
+{
+    public class Logger
+    {
+        private const string LOGFILE_PATH = "log";
+
+        public static void Log(string message)
+        {
+            if (!File.Exists(LOGFILE_PATH)) File.Create(LOGFILE_PATH).Close();
+
+            using var stream = new StreamWriter(LOGFILE_PATH);
+            stream.WriteLine(DateTime.Now.ToString("yyyy-M-d H:mm:ss", CultureInfo.InvariantCulture) + ": " + message);
+        }
+    }
+}
